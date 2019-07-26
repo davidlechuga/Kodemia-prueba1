69c24c17306e91dcec1496745fbcc9a60440a45f
davidlechuga <david.lechuga@lakmisystems.com.mx>
2019-07-26 16:54:52 -0500

    new

---
+++ js/java.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+            /* ABRIO LISTA  CLASS= "TOGGLE " */
+    $('.menu').click(function(){
+        $('.secciones').toggleClass('active');
+    });
+
+             /*ABRIO LISTA  CLASS= "TOGGLE "  */
+
+    $('.menu').click(function(){
+        $('.secciones .active').toggleClass('secciones');
+    });    
+    
+
+
+});
+
+
